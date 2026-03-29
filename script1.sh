d35e79f406e4c303c153038da9e8dd3b5e4b5da9
monesai444-hash <monesai444@gmail.com>
2026-03-29 21:46:20 +0530

    script1.sh

--- script1.sh
+++ script1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Script 1: System Identity Report
-# Author: Prathamesh Kumar | Course: Open Source Software
+# Author: Sai Rajendra Mone | Course: Open Source Software
 
 # --- Variables ---
-STUDENT_NAME="Prathamesh Kumar"
+STUDENT_NAME="Sai Rajendra Mone"
 SOFTWARE_CHOICE="Visual Studio Code"
 
 # --- System Info ---
